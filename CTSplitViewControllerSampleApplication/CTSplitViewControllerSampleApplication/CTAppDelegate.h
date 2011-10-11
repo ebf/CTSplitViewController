4c08da82784419ed2c365754ccced5eb812fd9d5
Oliver Letterer <oliver.letterer@googlemail.com>
2011-10-11 10:11:55 +0200

    initial project release

---
+++ CTSplitViewControllerSampleApplication/CTSplitViewControllerSampleApplication/CTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CTAppDelegate.h
+//  CTSplitViewControllerSampleApplication
+//
+//  Created by Oliver Letterer on 11.10.11.
+//  Copyright (c) 2011 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CTSplitViewControllerSampleApplication/CTSplitViewControllerSampleApplication/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CTSplitViewControllerSampleApplication
+//
+//  Created by Oliver Letterer on 11.10.11.
+//  Copyright (c) 2011 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CTAppDelegate class]));
+    }
+}
